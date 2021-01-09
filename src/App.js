d63fa9d7635f3be0e94603e71fe3e1b19a8aa38d
ThiCode126 <59418646+ThiCode126@users.noreply.github.com>
2021-01-09 14:18:35 +0100

    theme

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import React, { useState, useEffect } from 'react';
+
 
 function App() {
+
+  //Theme
+  const [ isDark, setIsDark ] = useState(false);
+
+  useEffect(() => {
+    const classCard = isDark ? 'dark' : 'light';
+    document.body.classList = `${classCard}-theme`;
+  }, [isDark]);
+
+  const handleTheme = () => {
+    setIsDark(!isDark);
+  }
+
   return (
     <div>
+      <button type="button" onClick={handleTheme} > Theme </button>
     </div>
   );
 }
